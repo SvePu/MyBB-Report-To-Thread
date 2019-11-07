bde1793dff3bfe6d11310bfd99447b7bd3af3e01
Sven <svp1702@freenet.de>
2019-11-07 23:53:57 +0100

    main plugin changes for #3

--- UPLOAD/inc/plugins/reporttothread.php
+++ UPLOAD/inc/plugins/reporttothread.php
@@ -14,7 +14,7 @@ else
 {
     $plugins->add_hook('report_do_report_end', 'reporttothread_run');
     $plugins->add_hook('class_moderation_delete_thread', 'reporttothread_delete_thread_from_cache');
-    $plugins->add_hook('global_start', 'reporttothread_load_lang');
+    $plugins->add_hook('report_start', 'reporttothread_load_lang');
 }
 
 function reporttothread_info()
@@ -27,7 +27,7 @@ function reporttothread_info()
         "website"       => "https://github.com/SvePu/MyBB_Report-To-Thread",
         "author"        => "SvePu",
         "authorsite"    => "https://github.com/SvePu",
-        "version"       => "1.2",
+        "version"       => "1.3",
         "codename"      => "reporttothread",
         "compatibility" => "18*"
     );
@@ -127,70 +127,114 @@ function reporttothread_run()
 {
     global $db, $mybb, $report_type, $lang, $cache, $session;
 
-    if($mybb->settings['reporttothread_enable'] == 1 && $report_type == 'post')
+    if($mybb->settings['reporttothread_enable'] != 1)
     {
-        if(empty($mybb->settings['reporttothread_fid']) || $mybb->settings['reporttothread_fid'] == "-1")
+        return;
+    }
+
+    if(empty($mybb->settings['reporttothread_fid']) || $mybb->settings['reporttothread_fid'] == "-1" || $mybb->settings['reporttothread_fid'] < 1)
+    {
+        return;
+    }
+
+    $forum_cache = $cache->read("forums");
+    if($mybb->settings['reporttothread_fid'] >= 1 && $forum_cache[$mybb->settings['reporttothread_fid']]['type'] == "c")
+    {
+        return;
+    }
+
+    $lang->load('report');
+    $lang->load('reporttothread');
+
+    $rid = $mybb->get_input('reason', MyBB::INPUT_INT);
+    $query = $db->simple_select("reportreasons", "title,extra", "rid = '{$rid}'");
+    $reasons = $db->fetch_array($query);
+
+    $reason = htmlspecialchars_uni($lang->parse($reasons['title']));
+
+    $comment = "";
+    if($reasons['extra'])
+    {
+        if(!empty($mybb->get_input('comment')))
         {
-            return;
+            $comment = $lang->sprintf(htmlspecialchars_uni($lang->reporttothread_comment), $mybb->user['username'], trim($mybb->get_input('comment')))."\n";
         }
+    }
 
-        $forum_cache = $cache->read("forums");
-        if($mybb->settings['reporttothread_fid'] >= 1 && $forum_cache[$mybb->settings['reporttothread_fid']]['type'] == "c")
-        {
-            return;
-        }
+    switch ($report_type) {
+        case 'post':
+            $post = get_post($mybb->get_input('pid', MyBB::INPUT_INT));
+            $reported_id = $post['pid'];
+            $rtype = htmlspecialchars_uni($lang->reporttothread_type_post);
 
-        $lang->load('report');
-        $lang->load('reporttothread');
+            $thread = get_thread($post['tid']);
+            $forum = get_forum($thread['fid']);
 
-        $post = get_post($mybb->get_input('pid', MyBB::INPUT_INT));
-        $reported_pid = $post['pid'];
-
-        $thread = get_thread($post['tid']);
-        $forum = get_forum($thread['fid']);
-
-        $rid = $mybb->get_input('reason', MyBB::INPUT_INT);
-        $query = $db->simple_select("reportreasons", "title,extra", "rid = '{$rid}'");
-        $reasons = $db->fetch_array($query);
-
-        $reason = htmlspecialchars_uni($lang->parse($reasons['title']));
-
-        $comment = "";
-        if($reasons['extra'])
-        {
-            if(!empty($mybb->get_input('comment')))
+            $pd_thread_link = "[url=" . $mybb->settings['bburl'] . "/" . get_thread_link($thread['tid']) . "]" . $thread['subject'] . "[/url]";
+            $pd_forum_link = "[url=" . $mybb->settings['bburl'] . "/" . get_forum_link($thread['fid']) . "]" . $forum['name'] . "[/url]";
+            if(my_strlen($post['message']) > 1000)
             {
-                $comment = "[quote=\"" . $mybb->user['username'] . "\" dateline=\"" . time() . "\"]" . trim($mybb->get_input('comment')) ."[/quote]\n";
+                $post['message'] = my_substr($post['message'], 0, 997)."...";
             }
-        }
+            $pd_reported_post = "[quote=\"" . $post['username'] . "\" pid=\"" . $post['pid'] . "\" dateline=\"" . $post['dateline'] . "\"]" . $post['message'] . "[/quote]";
+            $message = $lang->sprintf(htmlspecialchars_uni($lang->reporttothread_message_post), $mybb->user['username'], $rtype, $pd_thread_link, $pd_forum_link, $reason, $comment, $pd_reported_post);
+            break;
+        case 'profile':
+            $user = get_user($mybb->get_input('pid', MyBB::INPUT_INT));
+            $reported_id = $user['uid'];
+            $rtype = htmlspecialchars_uni($lang->reporttothread_type_profile);
+            $userlink = "[url=" . $mybb->settings['bburl'] . "/member.php?action=profile&uid=" . $user['uid'] . "]" . $user['username'] . "[/url]";
+            $message = $lang->sprintf(htmlspecialchars_uni($lang->reporttothread_message_profile), $mybb->user['username'], $rtype, $userlink, $reason, $comment);
+            break;
+        case 'reputation':
+            $query1 = $db->simple_select("reputation", "*", "rid = '".$mybb->get_input('pid', MyBB::INPUT_INT)."'");
+            $reputation = $db->fetch_array($query1);
+            $reported_id = $reputation['rid'];
+            $rtype = htmlspecialchars_uni($lang->reporttothread_type_reputation);
+            $getuser = get_user($reputation['uid']);
+            $getuserlink = "[url=" . $mybb->settings['bburl'] . "/member.php?action=profile&uid=" . $getuser['uid'] . "]" . $getuser['username'] . "[/url]";
+            $adduser = get_user($reputation['adduid']);
+            $adduserlink = "[url=" . $mybb->settings['bburl'] . "/member.php?action=profile&uid=" . $adduser['uid'] . "]" . $adduser['username'] . "[/url]";
+            if($reputation['pid'] > 0)
+            {
+                $reppost = get_post($reputation['pid']);
+                $reppostlink = $getuser['username'] . "'s " .$lang->reporttothread_type_post . ": [url=" . $mybb->settings['bburl'] . "/" . get_post_link($reputation['pid']) . "#pid" . $reputation['pid'] . "]" . $reppost['subject'] . "[/url]";
+            }
+            else
+            {
+                $reppostlink = $lang->reporttothread_type_profile;
+            }
+            $reputation_comment = '';
+            if(!empty($reputation['comments']))
+            {
+                $reputation_comment = "\n[quote=\"" . $adduser['username'] . "\" dateline=\"" . $reputation['dateline'] . "\"]" . htmlspecialchars_uni($reputation['comments']) . "[/quote]";
+            }
+            $message = $lang->sprintf(htmlspecialchars_uni($lang->reporttothread_message_reputation), $mybb->user['username'], $rtype, $getuserlink, $reason, $comment, $reputation['reputation'],$adduserlink, $reppostlink, $reputation_comment);
+            break;
+    }
 
-        $pd_thread_link = "[url=" . $mybb->settings['bburl'] . "/" . get_thread_link($thread['tid']) . "]" . $thread['subject'] . "[/url]";
-        $pd_forum_link = "[url=" . $mybb->settings['bburl'] . "/" . get_forum_link($thread['fid']) . "]" . $forum['name'] . "[/url]";
-        $pd_reported_post = "[quote=\"" . $post['username'] . "\" pid=\"" . $post['pid'] . "\" dateline=\"" . $post['dateline'] . "\"]" . $post['message'] . "[/quote]";
+    $subject = $lang->sprintf(htmlspecialchars_uni($lang->reporttothread_subject), $rtype, $mybb->user['username']);
 
-        $subject = $lang->sprintf(htmlspecialchars_uni($lang->reporttothread_subject), $mybb->user['username']);
-        $message = $lang->sprintf(htmlspecialchars_uni($lang->reporttothread_message), $mybb->user['username'], $pd_thread_link, $pd_forum_link, $reason, $comment, $pd_reported_post);
+    $reportedthread = array();
+    $reportedthread = $cache->read('reporttothread');
+    $find_tid = reporttothread_searchForId($reported_id, $report_type, $reportedthread);
+    if($find_tid)
+    {
+        $thread_info = reporttothread_build_post($find_tid, $subject, $message);
+    }
+    else
+    {
+        $thread_info = reporttothread_build_thread($subject, $message, $reported_id, $report_type);
+    }
 
-        $reportedthread = array();
-        $reportedthread = $cache->read('reporttothread');
-        if(array_key_exists($reported_pid, $reportedthread))
-        {
-            $thread_info = reporttothread_build_post($reportedthread[$reported_pid], $subject, $message);
-        }
-        else
-        {
-            $thread_info = reporttothread_build_thread($subject, $message, $reported_pid);
-        }
-
-        if($thread_info && $mybb->settings['reporttothread_modcp'] != 0)
-        {
-            $db->update_query("reportedcontent", array('reportstatus' => 1), "id = '{$post['pid']}'");
-            $cache->update_reportedcontent();
-        }
+    if($thread_info && $mybb->settings['reporttothread_modcp'] != 0)
+    {
+        $db->update_query("reportedcontent", array('reportstatus' => 1), "id = '{$reported_id}'");
+        $cache->update_reportedcontent();
     }
 }
 
-function reporttothread_build_thread($subject, $message, $reported_pid)
+function reporttothread_build_thread($subject, $message, $reported_id, $report_type)
 {
     global $mybb, $session;
     require_once MYBB_ROOT."inc/datahandlers/post.php";
@@ -218,7 +262,7 @@ function reporttothread_build_thread($subject, $message, $reported_pid)
         if($thread_info)
         {
             $tid = $thread_info['tid'];
-            reporttothread_cache($reported_pid, $tid);
+            reporttothread_cache($tid, $reported_id, $report_type);
         }
         return $thread_info;
     }
@@ -254,19 +298,12 @@ function reporttothread_build_post($tid, $subject, $message)
     }
 }
 
-function reporttothread_cache($pid, $tid)
+function reporttothread_cache($tid, $reported_id, $report_type)
 {
     global $cache;
     $reportedthread = array();
     $reportedthread = $cache->read('reporttothread');
-    if($reportedthread)
-    {
-        $reportedthread += [$pid => $tid];
-    }
-    else
-    {
-        $reportedthread = [$pid => $tid];
-    }
+    $reportedthread[$tid] = array('id' => $reported_id, 'type' => $report_type);
     $cache->update('reporttothread',$reportedthread);
 }
 
@@ -275,7 +312,7 @@ function reporttothread_delete_thread_from_cache($tid)
     global $cache;
     $reportedthread = array();
     $reportedthread = $cache->read('reporttothread');
-    $reportedthread = array_diff($reportedthread,array($tid));
+    unset($reportedthread[$tid]);
     $cache->update('reporttothread',$reportedthread);
 }
 
@@ -289,4 +326,16 @@ function reporttothread_load_lang()
 {
     global $lang;
     $lang->load('reporttothread');
+}
+
+function reporttothread_searchForId($reported_id, $report_type, $array)
+{
+   foreach ($array as $key => $val)
+   {
+       if ($val['id'] == $reported_id && $val['type'] == $report_type)
+       {
+           return $key;
+       }
+   }
+   return false;
 }
